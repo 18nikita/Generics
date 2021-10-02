d9added4f3ced6ca69ad32e2041ac91686e9a3d8
unknown <rajputnikita5666@gmail.com>
2021-10-02 23:57:45 +0530

    Added:Maximum Float number program

--- src/com/bridgelabz/generics/Generics.java
+++ src/com/bridgelabz/generics/Generics.java
@@ -6,25 +6,25 @@ public class Generics {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Generics");
-		Scanner sc= new Scanner(System.in);
-		System.out.println("Enter first integer");
-		int a=sc.nextInt();
-		System.out.println("Enter second integer");
-		int b=sc.nextInt();
-		System.out.println("Enter third integer");
-		int c=sc.nextInt();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter first Float");
+		float a = sc.nextFloat();
+		System.out.println("Enter second Float");
+		float b = sc.nextFloat();
+		System.out.println("Enter third Float");
+		float c = sc.nextFloat();
 		sc.close();
 		getMax(a, b, c);
 	}
 
-	public static Integer getMax(Integer a, Integer b, Integer c) {
-		int max = a;
+	public static Float getMax(Float a, Float b, Float c) {
+		Float max = a;
 		if (b.compareTo(a) > 0 && b.compareTo(c) > 0) {
 			max = b;
 		} else if (c.compareTo(a) > 0)
 			max = c;
 
-		System.out.println("The maximum no. is: "+ max );
+		System.out.println("The maximum no. is: " + max);
 		return max;
 	}
 }
