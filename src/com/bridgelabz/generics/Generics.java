761057519166409c8320221f929525be38636f77
unknown <rajputnikita5666@gmail.com>
2021-10-03 00:06:56 +0530

    Added:Long string program

--- src/com/bridgelabz/generics/Generics.java
+++ src/com/bridgelabz/generics/Generics.java
@@ -7,18 +7,18 @@ public class Generics {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Generics");
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter first Float");
-		float a = sc.nextFloat();
-		System.out.println("Enter second Float");
-		float b = sc.nextFloat();
-		System.out.println("Enter third Float");
-		float c = sc.nextFloat();
+		System.out.println("Enter first String");
+		String a = sc.next();
+		System.out.println("Enter second String");
+		String b = sc.next();
+		System.out.println("Enter third String");
+		String c = sc.next();
 		sc.close();
 		getMax(a, b, c);
 	}
 
-	public static Float getMax(Float a, Float b, Float c) {
-		Float max = a;
+	public static String getMax(String a, String b, String c) {
+		String max = a;
 		if (b.compareTo(a) > 0 && b.compareTo(c) > 0) {
 			max = b;
 		} else if (c.compareTo(a) > 0)
