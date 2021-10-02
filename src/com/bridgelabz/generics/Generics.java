4b849514a32522f4cdb2390715227a05f1f27fa2
unknown <rajputnikita5666@gmail.com>
2021-10-02 23:36:49 +0530

    Added:Maximum Integer program

--- src/com/bridgelabz/generics/Generics.java
+++ src/com/bridgelabz/generics/Generics.java
@@ -1,10 +1,30 @@
 package com.bridgelabz.generics;
 
+import java.util.Scanner;
+
 public class Generics {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Generics");
-
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter first integer");
+		int a=sc.nextInt();
+		System.out.println("Enter second integer");
+		int b=sc.nextInt();
+		System.out.println("Enter third integer");
+		int c=sc.nextInt();
+		sc.close();
+		getMax(a, b, c);
 	}
 
+	public static Integer getMax(Integer a, Integer b, Integer c) {
+		int max = a;
+		if (b.compareTo(a) > 0 && b.compareTo(c) > 0) {
+			max = b;
+		} else if (c.compareTo(a) > 0)
+			max = c;
+
+		System.out.println("The maximum no. is: "+ max );
+		return max;
+	}
 }
