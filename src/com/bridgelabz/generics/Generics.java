90b87a1ce8e744b0d735631fdeb1f729d0ebf22a
unknown <rajputnikita5666@gmail.com>
2021-10-03 22:22:33 +0530

    Added:Refactor code for Previous UCs

--- src/com/bridgelabz/generics/Generics.java
+++ src/com/bridgelabz/generics/Generics.java
@@ -1,30 +1,21 @@
 package com.bridgelabz.generics;
 
-import java.util.Scanner;
-
 public class Generics {
-
-	public static void main(String[] args) {
-		System.out.println("Welcome to Generics");
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter first String");
-		String a = sc.next();
-		System.out.println("Enter second String");
-		String b = sc.next();
-		System.out.println("Enter third String");
-		String c = sc.next();
-		sc.close();
-		getMax(a, b, c);
+	public static <T extends Comparable<T>> T maximum(T value1, T value2, T value3) {
+		T max = value1;
+		if (value2.compareTo(max) > 0)
+			max = value2;
+		if (value3.compareTo(max) > 0)
+			max = value3;
+		return max;
 	}
 
-	public static String getMax(String a, String b, String c) {
-		String max = a;
-		if (b.compareTo(a) > 0 && b.compareTo(c) > 0) {
-			max = b;
-		} else if (c.compareTo(a) > 0)
-			max = c;
-
-		System.out.println("The maximum no. is: " + max);
-		return max;
+	public static void main(String args[]) {
+		System.out.printf("Maximum of %d, %d and %d is %d\n\n", 18, 20, 15, maximum(18, 20, 15));  //printf("%d"\n); //tells to print integer n.
+		
+		System.out.printf("Maximum of %.1f, %.1f and %.1f is %.1f\n\n", 1.1, 2.2, 3.3, maximum(1.1, 2.2, 3.3));
+		
+		System.out.printf("Maximum of %s, %s and %s is %s\n", "XYZ", "ABCD", "PQRST",
+				maximum("XYZ", "ABCD", "PQRST" ));
 	}
 }
