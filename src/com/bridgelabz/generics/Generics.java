24bea71656e5f7c0dc9c3eedd1f6d81472aa6627
unknown <rajputnikita5666@gmail.com>
2021-10-04 14:06:17 +0530

    Added:Refactoring Using class generic method

--- src/com/bridgelabz/generics/Generics.java
+++ src/com/bridgelabz/generics/Generics.java
@@ -1,21 +1,37 @@
 package com.bridgelabz.generics;
 
-public class Generics {
-	public static <T extends Comparable<T>> T maximum(T value1, T value2, T value3) {
-		T max = value1;
-		if (value2.compareTo(max) > 0)
-			max = value2;
-		if (value3.compareTo(max) > 0)
-			max = value3;
-		return max;
-	}
+import javax.swing.Box;
+
+
+public class Generics<E extends Comparable> {
+   E value1,value2,value3;
+
+
+    public Generics(E value1, E value2, E value3) {
+        this.value1 = value1;
+        this.value2 = value2;
+        this.value3 = value3;
+    }
+    public E testMaximum(){
+        E testMax= testMaximum(value1,value2,value3);
+        return  testMax;
+    }
+
+    public static <E extends Comparable> E testMaximum(E value1, E value2, E value3) {
+        E max = value1;
+        if (value2.compareTo(value1) > 0 && value2.compareTo(value3) > 0)
+            max = value2;
+        else if (value3.compareTo(value2) > 0 && value3.compareTo(value1) > 0)
+            max = value3;
+        System.out.println(max);
+        return max;
+    }
 
 	public static void main(String args[]) {
-		System.out.printf("Maximum of %d, %d and %d is %d\n\n", 18, 20, 15, maximum(18, 20, 15));  //printf("%d"\n); //tells to print integer n.
+	
+		Object object = new Object();
+		object.textMaximum(value1, value2, value3);
 		
-		System.out.printf("Maximum of %.1f, %.1f and %.1f is %.1f\n\n", 1.1, 2.2, 3.3, maximum(1.1, 2.2, 3.3));
-		
-		System.out.printf("Maximum of %s, %s and %s is %s\n", "XYZ", "ABCD", "PQRST",
-				maximum("XYZ", "ABCD", "PQRST" ));
 	}
+    
 }
